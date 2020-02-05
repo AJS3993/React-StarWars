9bf34cec040815efb6a8b36f0c07f4df5184c09f
Andrew Smith <ajs3993@gmail.com>
2020-02-05 15:18:39 -0600

    starship list moved into seperate component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 import { Route, Switch, Link } from 'react-router-dom';
 import { getAllStarships } from './services/sw-api';
 import StarshipPage from './pages/StarshipPage/StarshipPage';
+import StarshipListPage from './pages/StarshipListPage/StarshipListPage'
 
 class App extends Component {
-  
-  state = {
-    starships: []
-  };
-
-  getStarship = (idx) => {
-    return this.state.starships[idx];
-  }
-
-  async componentDidMount() {
-    const starships = await getAllStarships();
-    this.setState({ starships: starships.results });
-  }
   
   render() {
     return (
@@ -31,31 +19,8 @@ class App extends Component {
           <div>Planets</div>
         
         </div>
-        
-        <Switch>
 
-          <Route exact path='/' render={() => 
-            <section>
-              {this.state.starships.map(starship => 
-                <Link
-                  to={{
-                    pathname: '/starships',
-                    state: starship
-                  }}
-                  key={starship.name}
-                >
-                  {starship.name}
-                </Link>
-              )}
-            </section>
-          }/>
-          <Route path='/starships' render={({location}) => 
-            <StarshipPage
-              location={location}
-            />
-          }/>
-
-        </Switch>
+        <StarshipListPage />
 
       </div>
     );
---
+++ src/pages/StarshipListPage/StarshipListPage.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import { Route, Switch, Link } from 'react-router-dom';
+import StarshipPage from '../StarshipPage/StarshipPage';
+import { getAllStarships } from '../../services/sw-api';
+
+class StarshipListPage extends Component{
+
+    state = {
+        starships: []
+      };
+
+      getStarship = (idx) => {
+        return this.state.starships[idx];
+      }
+    
+      async componentDidMount() {
+        const starships = await getAllStarships();
+        this.setState({ starships: starships.results });
+      }
+
+    render(){
+        return(
+            <div>
+                
+                <Switch>
+
+<Route exact path='/' render={() => 
+  <section>
+    {this.state.starships.map(starship => 
+      <Link
+        to={{
+          pathname: '/starships',
+          state: starship
+        }}
+        key={starship.name}
+      >
+        {starship.name}
+      </Link>
+    )}
+  </section>
+}/>
+<Route path='/starships' render={({location}) => 
+  <StarshipPage
+    location={location}
+  />
+}/>
+
+</Switch>
+                
+            </div>
+        )
+    }
+}
+export default StarshipListPage
