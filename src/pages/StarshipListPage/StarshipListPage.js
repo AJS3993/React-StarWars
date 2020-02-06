388fb0589e8302d80611c4573642ee2aa1fd2d5f
Andrew Smith <ajs3993@gmail.com>
2020-02-06 15:17:46 -0600

    ship info rendered

--- src/pages/StarshipListPage/StarshipListPage.js
+++ src/pages/StarshipListPage/StarshipListPage.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { getAllStarships } from '../../services/sw-api';
-// import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-// import StarshipPage from '../StarshipPage/StarshipPage';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import StarshipPage from '../StarshipPage/StarshipPage';
 import './StarshipListPage.css'
 
 class StarshipListPage extends Component{
@@ -22,24 +22,29 @@ class StarshipListPage extends Component{
 
   render(){
     return(
-      <div>
+      <Router>
         <h1 className='shiph1'>Ships</h1>
 
         <section className='shipsection'>
           {this.state.starships.map((starship, idx) => 
             <div className='ship' key={starship.name}>
               {starship.name}
+              <Link key={starship.name} to={`/starships/${idx}`}>test</Link>
 
-              
-            
-
-
-
-             
             </div>
+            
           )}
-        </section>     
-      </div>
+        </section>  
+    
+          
+          <Switch>
+              <Route path='/starships/:idx' render={(props) =>
+              <StarshipPage {...props} getStarship={this.getStarship}/>
+              }/>
+          </Switch>
+      
+      
+      </Router>
     )
   }
 }
--- src/pages/StarshipPage/StarshipPage.jsx
+++ src/pages/StarshipPage/StarshipPage.jsx
@@ -1,15 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './StarshipPage.css';
 
-class StarshipPage extends Component{
-
-  render(){
-    return(
-      <div>
-      hi
-      </div>
-    )
-  }
-}
+const StarshipPage = (props) => {
+  
+  const starship = props.getStarship(props.match.params.idx);
+  
+  return (
+    <div className='StarshipPage'>
+      {starship ?
+        <div className='StarshipPage-starship'>
+          <span>NAME:</span>
+          <span>{starship.name}</span>
+          <span>MODEL:</span>
+          <span>{starship.model}</span>
+          <Link to='/'>RETURN</Link>
+        </div>
+        :
+        <h3>Loading...</h3>
+      }
+    </div>
+  );
+};
 
 export default StarshipPage;
